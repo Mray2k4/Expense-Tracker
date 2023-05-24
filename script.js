6f18d740cbd472cbbb1dfc53fac701aada2eb3f8
Marque Ray <mray2k4@yahoo.com>
2023-05-23 21:13:07 -0700

    added code to html, css and js

---
+++ script.js
@@ -0,0 +1,36 @@
+const btnAdd = document.getElementById('btnAdd');
+const table = document.getElementById('table');
+
+//const dateInput = document.getElementById('date');
+const descriptInput = document.getElementById('descript');
+const categoryInput = document.getElementById('category');
+const amountInput = document.getElementById('amount');
+//const remove = document.getElementById('remove');
+
+btnAdd.addEventListener('click', function() {
+  const descript = descriptInput.value;
+  const category = categoryInput.value;
+  const amount = amountInput.value;
+  const remove = document.getElementById('remove');
+  
+  const form = `<tr>
+                  <td></td>
+                  <td>${descript}</td>
+                  <td>${category}</td>
+                  <td>${amount}</td>
+                  <td><button class='remove'>X</button></td>
+                </tr>`;
+  
+  table.innerHTML += form;
+})
+
+function deleteData(e) {
+  if(!e.target.classList.contains('remove')) {
+    return;
+  }
+  
+  const btn = e.target;
+  btn.remove();
+}
+
+remove.addEventListener('click', deleteData());

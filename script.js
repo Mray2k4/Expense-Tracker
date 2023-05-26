8b9143126f2a4bdbd333dcba664cf1665b990a11
Marque Ray <mray2k4@yahoo.com>
2023-05-25 21:54:51 -0700

    added background color and info disappears after you add

--- script.js
+++ script.js
@@ -23,11 +23,14 @@ btnAdd.addEventListener('click', function() {
                   <td>${date}</td>
                   <td>${descript}</td>
                   <td>${category}</td>
-                  <td>$${amount}</td>
+                  <td>${'$'+ amount}</td>
                   <td><input type="button" value="Delete" onclick="deleteRow(this)"></td>
                 </tr>`;
 
-            
+                dateInput.value = "";
+                descriptInput.value = "";
+                categoryInput.value = "";
+                amountInput.value = "";
   
                 
   
